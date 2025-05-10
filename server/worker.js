919a1d4d1215b247c6474c2f8571ff69461e57cc
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-05-10 03:52:00 -0400

    Added Code for: Worker take pdf with information like: filename, destination, path

--- server/worker.js
+++ server/worker.js
@@ -1,7 +1,24 @@
-import { Worker, Job } from 'bullmq';
+import { Worker } from "bullmq";
 
-const worker = new Worker(file-upload-queue, async (job) => {
-  console.log(`Job:`, job.data);
-},
-{ concurrency: 100 }
+const worker = new Worker(
+  "file-upload-queue",
+  async (job) => {
+    console.log(`Job:`, job.data);
+    const data = JSON.parse(job.data);
+    /*
+        Take file path
+        read PDF from path
+        chunk the pdf 
+        call the OpenAI embedding model for every chunk
+        store the chunk in QDrant DB
+    */
+   
+  },
+  {
+    concurrency: 100,
+    connection: {
+      host: "localhost",
+      port: 6379,
+    },
+  }
 );
